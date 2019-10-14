59507aee2b6f882b33a7ff2448740117ac33c902
tett23 <tett23@gmail.com>
2019-10-14 18:56:10 +0900

    Parse argv

--- src/main.go
+++ src/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 
 	"github.com/tett23/kinsro/src/commands"
 	"github.com/tett23/kinsro/src/config"
@@ -9,18 +11,38 @@ import (
 )
 
 func main() {
-	conf := config.GetConfig()
+	flagSet := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+
+	command := os.Args[1]
+	flagSet.Parse(os.Args[2:])
+
+	var f func(conf *config.Config) error
+	switch command {
+	case "build":
+		f = build
+	default:
+		panic(fmt.Sprintf("Unexpected command. command=%v", command))
+	}
+
+	err := f(config.GetConfig())
+	if err != nil {
+		fmt.Printf("%+v\n", err)
+		panic(err)
+	}
+}
+
+func build(conf *config.Config) error {
 	storagePaths := conf.StoragePaths
 
 	vindex, err := commands.BuildVIndex(storagePaths)
 	if err != nil {
-		fmt.Printf("%+v\n", err)
-		panic(err)
+		return err
 	}
 
 	err = writer.CreateNewIndexFile(conf, vindex)
 	if err != nil {
-		fmt.Printf("%+v\n", err)
-		panic(err)
+		return err
 	}
+
+	return nil
 }
